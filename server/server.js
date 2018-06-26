1b6fa818ed8a9d9b6d093eb54c86ddb549f629dc
hussu97 <h_abbasi97@hotmail.com>
2018-06-26 10:53:55 +0400

    Added socket.io functionality

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
-const path    = require('path'),
-      express = require('express');
-
-const app = express();
+const path     = require('path'),
+      http     = require('http'),
+      express  = require('express'),
+      socketIO = require('socket.io');
+var a =0;
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname,'../public');
 
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  a=a+1;
+  console.log(`New user connected: ${a}`);
 
-app.listen(port, function () {
+  socket.on('disconnect', () => {
+    a=a-1;
+    console.log(`User was disconnected: ${a}`);
+
+  })
+});
+
+server.listen(port, function () {
   console.log(`Server Starts on ${port}`);
 });
